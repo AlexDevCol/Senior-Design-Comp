b479c69d0e4a18db837e6ca016ab630c001244d0
AlexDevCol <luigi.gonzalez@live.com>
2025-10-25 10:55:46 -0400

    Updated IR Logic

--- senior_design/senior_design.ino
+++ senior_design/senior_design.ino
@@ -1,9 +1,192 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+ * CQI 2026 Robot Software - Line Following with RemoteXY Control
+ * ESP8266 NodeMCU with RemoteXY and line following capabilities
+ */
 
+// RemoteXY include library
+#define REMOTEXY_MODE__WIFI_POINT
+#include <ESP8266WiFi.h>
+#include <RemoteXY.h>
+
+// RemoteXY connection settings
+#define REMOTEXY_WIFI_SSID "RobotWiFi"
+#define REMOTEXY_WIFI_PASSWORD "robot123"
+#define REMOTEXY_SERVER_PORT 6377
+
+// RemoteXY GUI configuration  
+#pragma pack(push, 1)  
+uint8_t RemoteXY_CONF[] =   // 71 bytes
+  { 255,4,0,1,0,64,0,19,0,0,0,0,31,1,106,200,1,1,5,0,
+  4,5,7,94,17,176,5,26,4,8,170,94,17,176,5,26,4,16,54,14,
+  81,32,2,26,4,45,54,14,81,32,2,26,74,32,34,41,12,13,2,30,
+  37,64,83,116,114,105,110,103,32,49,0 };
+ 
+// this structure defines all the variables and events of your control interface
+struct {
+    // input variables
+  int8_t slider_01; // motorL
+  int8_t slider_02; // motor2
+  int8_t slider_03; // servo1
+  int8_t slider_04; // servo2
+
+    // output variables
+  uint8_t strings_01; // from 0 to 1
+
+    // other variable
+  uint8_t connect_flag;  // =1 if wire connected, else =0
+} RemoteXY;  
+#pragma pack(pop)
+
+// IR sensor pins
+#define IR_L 13  // D7
+#define IR_R 15  // D8
+
+// System states
+enum RobotState {
+  AUTONOMOUS,
+  REMOTE_CONTROL
+};
+
+// Global variables
+RobotState currentState = AUTONOMOUS;
+unsigned long lastStateChange = 0;
+bool manualOverride = false;
+
+// Motor control structure
+struct Motor {
+  const char* name;
+  uint8_t speedPin;
+  uint8_t dir1Pin;
+  uint8_t dir2Pin;
+  int duty;
+  bool dirState1;
+  bool dirState2;
+
+  // Initialize
+  void begin() {
+    pinMode(speedPin, OUTPUT);
+    pinMode(dir1Pin, OUTPUT);
+    pinMode(dir2Pin, OUTPUT);
+  }
+
+  // Update PWM and direction
+  void update() {
+    if (duty < 0) {
+      dirState1 = LOW;
+      dirState2 = !dirState1;
+    }
+    if (duty > 0) {
+      dirState1 = HIGH;
+      dirState2 = !dirState1;
+    }
+    if (abs(duty) < 10) {
+      dirState1 = LOW;
+      dirState2 = LOW;
+    }
+
+    digitalWrite(dir1Pin, dirState1);
+    digitalWrite(dir2Pin, dirState2);
+    analogWrite(speedPin, abs(duty));
+  }
+};
+
+// Motor objects - using your pin configuration
+Motor motorL = {"L", 12, 14, 2};  // speedPin, dir1Pin, dir2Pin
+Motor motorR = {"R", 5, 4, 0};   // speedPin, dir1Pin, dir2Pin
+
+void setup() {
+  Serial.begin(115200);
+  
+  // Initialize RemoteXY
+  RemoteXY_Init();
+  
+  // Initialize motors
+  motorL.begin();
+  motorR.begin();
+  
+  // Initialize IR sensors
+  pinMode(IR_L, INPUT);
+  pinMode(IR_R, INPUT);
+  
+  Serial.println("CQI 2026 Robot Started");
+  Serial.println("WiFi: RobotWiFi, Password: robot123");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  RemoteXY_Handler();
+  
+  // Check for manual override (button press or slider movement)
+  if (RemoteXY.slider_01 != 0 || RemoteXY.slider_02 != 0) {
+    currentState = REMOTE_CONTROL;
+    manualOverride = true;
+    lastStateChange = millis();
+  }
+  
+  // Auto-return to autonomous after 5 seconds of no manual input
+  if (manualOverride && (millis() - lastStateChange > 5000)) {
+    currentState = AUTONOMOUS;
+    manualOverride = false;
+    Serial.println("Returning to autonomous mode");
+  }
+  
+  if (currentState == AUTONOMOUS) {
+    runAutonomousMode();
+  } else {
+    runRemoteControlMode();
+  }
+  
+  delay(10);
 }
+
+void runAutonomousMode() {
+  uint16_t ir_l = digitalRead(IR_L);
+  uint16_t ir_r = digitalRead(IR_R);
+  
+  // Line following logic
+  if ((ir_l == LOW) && (ir_r == LOW)) {
+    // Both sensors on white - go forward
+    motorL.duty = 150;
+    motorR.duty = 150;
+    Serial.println("Forward");
+    
+  } else if ((ir_l == HIGH) && (ir_r == HIGH)) {
+    // Both sensors on black - stop
+    motorL.duty = 0;
+    motorR.duty = 0;
+    Serial.println("Stop");
+    
+  } else if (ir_l == LOW) {
+    // Left sensor on white, right on line - turn right
+    motorL.duty = 150;
+    motorR.duty = -100;
+    Serial.println("Turn Right");
+    
+  } else if (ir_r == LOW) {
+    // Right sensor on white, left on line - turn left
+    motorL.duty = -100;
+    motorR.duty = 150;
+    Serial.println("Turn Left");
+  }
+  
+  // Update motors
+  motorL.update();
+  motorR.update();
+}
+
+void runRemoteControlMode() {
+  int slider_llimit = -100;
+  int slider_ulimit = 100;
+  int speedLimit = 255;
+  
+  // Map slider values to motor duty
+  motorL.duty = map(RemoteXY.slider_01, slider_llimit, slider_ulimit, -speedLimit, speedLimit);
+  motorR.duty = map(RemoteXY.slider_02, slider_llimit, slider_ulimit, -speedLimit, speedLimit);
+  
+  // Update motors
+  motorL.update();
+  motorR.update();
+  
+  // Update status string
+  RemoteXY.strings_01 = 1; // Indicate remote control mode
+}
+
